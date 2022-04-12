bbcb8ee73e3bd6ddb84e145ccc3c5f2f43b8d71f
Dennis Osei-Wusu <sirdenniso20@gmail.com>
2022-04-11 22:53:09 -0500

    version 1

---
+++ Demo1/src/Basics.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Given - all input details
+		//when - Submit the API
+		
+
+	}
+
+}
